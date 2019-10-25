fed9a432f7b733987ad5a4fcb95cbcd5e83fa6d8
Benjamin Danjoux <benjamin.danjoux@gmail.com>
2019-10-25 16:24:56 +0200

    hey

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import time
 import urllib.request as request
 from xml.dom.minidom import parseString
-url = 'https://eyes.nasa.gov/dsn/data/dsn.xml'
 
-r = int(time.time() / 5)
-response = request.urlopen(url)
-xmldoc=parseString(response.read())
-print(xmldoc)
-targets = xmldoc.getElementsByTagName("target")
-print(targets)
-for target in targets:
-    name = target.getAttribute("name")
-    print(name)
+def isVgrOnline():
+    url = 'https://eyes.nasa.gov/dsn/data/dsn.xml'
+    response = request.urlopen(url)
+    xmldoc=parseString(response.read())
+    #print(xmldoc)
+    targets = xmldoc.getElementsByTagName("target")
+    #print(targets)
+    ret=False
+    for target in targets:
+        name = target.getAttribute("name")
+        if name=="VGR1" or name=="VGR2":
+            ret=True
+    return ret
+
+print("Is Vgr Online : "+str(isVgrOnline()))
+
